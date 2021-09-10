3ead303af9904e7190c0e41a08e2a1e1eb0ab22d
Densolator <raffaeld12@gmail.com>
2021-09-11 00:07:37 +0800

    Added functionality to hide the guide from HUD

--- GDFUNDA_GameJam/Assets/Scripts/Game Jam 2/PlayerController.cs
+++ GDFUNDA_GameJam/Assets/Scripts/Game Jam 2/PlayerController.cs
@@ -4,25 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    [SerializeField] private GameObject panel;
+    bool panelActive = false;
     //[SerializeField] public GameObject playerTrigger;
-    [SerializeField] public TimerController timeController;
+    //[SerializeField] public TimerController timeController;
     // Start is called before the first frame update
     void Start()
     {
-        timeController.BeginTimer();
-        Debug.Log("Start timer");
+        //timeController.BeginTimer();
+        //Debug.Log("Start timer");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("tab"))
+        {
+            if (panelActive)
+            {
+                panel.SetActive(false);
+                panelActive = false;
+            }
+            else
+            {
+                panel.SetActive(true);
+                panelActive = true;
+            }
+        }
     }
 
     private void OnTriggerEnter()
     {
-        timeController.EndTimer();
-        Debug.Log("Tapos na");
+        //timeController.EndTimer();
+        //Debug.Log("Tapos na");
     }
 }
