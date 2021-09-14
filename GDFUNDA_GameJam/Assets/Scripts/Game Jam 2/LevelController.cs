ac2224661d86fbe101e39d56aad4d1015597be91
Densolator <raffaeld12@gmail.com>
2021-09-14 14:13:42 +0800

    Added blackout effect after finishing level

--- GDFUNDA_GameJam/Assets/Scripts/Game Jam 2/LevelController.cs
+++ GDFUNDA_GameJam/Assets/Scripts/Game Jam 2/LevelController.cs
@@ -9,8 +9,9 @@ public class LevelController : MonoBehaviour
     [SerializeField] PlayerController PController;
     [SerializeField] TimerController TController;
     [SerializeField] GameObject FPSCamera;
-    [SerializeField] Text timerText;
+    [SerializeField] GameObject timerText;
     [SerializeField] NPCConversation endConvoManager;
+    [SerializeField] GameObject blackoutSquare;
     bool levelFinished;
     // Start is called before the first frame update
     void Start()
@@ -22,7 +23,7 @@ public class LevelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnCollisionEnter(Collision collision)
@@ -50,13 +51,57 @@ public class LevelController : MonoBehaviour
         levelFinished = true;
         ConversationManager.Instance.StartConversation(endConvoManager);
         lockPlayerMovement();
+        timerText.SetActive(false);
+        blackout();
     }
 
     public void beginLevel()
     {
         Debug.Log("test");
+        //blackin();
         TController.BeginTimer();
         FPSCamera.SetActive(true);
+        timerText.SetActive(true);
         unlockPlayerMovement();
+    }
+
+    public void blackin()
+    {
+        StartCoroutine(FadeBlackOutSquare(false));
+    }
+
+    public void blackout()
+    {
+        StartCoroutine(FadeBlackOutSquare());
+    }
+
+    public IEnumerator FadeBlackOutSquare(bool fadeToBlack = true, int fadeSpeed = 2)
+    {
+        Color objectColor = blackoutSquare.GetComponent<Image>().color;
+        float fadeAmount;
+
+        if(fadeToBlack)
+        {
+            Debug.Log("blackout");
+            while(blackoutSquare.GetComponent<Image>().color.a<255)
+            {
+                fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                blackoutSquare.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+        }
+        else 
+        {
+            while(blackoutSquare.GetComponent<Image>().color.a>0)
+            {
+                fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                blackoutSquare.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+        }
     }
 }
